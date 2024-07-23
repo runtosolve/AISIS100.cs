d1e5e0e52e1a9b8af695cd85f67a623207877081
Cris Moen <cris.moen@runtosolve.com>
2024-07-23 14:01:38 +0300

    add equation numbers to ChapterFFlexure

--- AISIS100/ChapterFFlexure.cs
+++ AISIS100/ChapterFFlexure.cs
@@ -2,102 +2,120 @@ namespace AISIS100;
 
 public static class ChapterFFlexure
 {
-    public static double EqF2_1__1(double Sfc, double Fn, double My)
+    public static double EqF2_1__1(double Sfc, double Fn, double My, Output? output = null)
     {
         var Mne = Math.Min(Sfc * Fn, My);
+        output?.AddResult("Mne", Mne, "Eq.F2.1-1");
         return Mne;
     }
     
-    public static double EqF2_1__2(double Sf, double Fy)
+    public static double EqF2_1__2(double Sf, double Fy, Output? output = null)
     {
         var My = Sf * Fy;
+        output?.AddResult("My", My, "Eq.F2.1-2");
         return My;
     }
 
-    public static double EqF2_1__3(double Fy)
+    public static double EqF2_1__3(double Fy, Output? output = null)
     {
         var Fn = Fy;
+        output?.AddResult("Fn", Fn, "Eq.F2.1-3");
         return Fn;
     }
     
-    public static double EqF2_1__4(double Fy, double Fcre)
+    public static double EqF2_1__4(double Fy, double Fcre, Output? output = null)
     {
         var Fn = 10.0 / 9.0 * Fy * (1 - (10 * Fy) / (36 * Fcre));
+        output?.AddResult("Fn", Fn, "Eq.F2.1-4");
         return Fn;
     }
     
-    public static double EqF2_1__5(double Fcre)
+    public static double EqF2_1__5(double Fcre, Output? output = null)
     {
         var Fn = Fcre;
+        output?.AddResult("Fn", Fn, "Eq.F2.1-5");
         return Fn;
     }
     
-    public static double EqF3_2__1(double Mne)
+    public static double EqF3_2__1(double Mne, Output? output = null)
     {
         var Mnl = Mne;
+        output?.AddResult("Mnl", Mnl, "Eq.F3.2-1");
         return Mnl;
     }
 
-    public static double EqF3_2__2(double Mcrl, double Mne, double My)
+    public static double EqF3_2__2(double Mcrl, double Mne, double My, Output? output = null)
     {
         var MneBar = Math.Min(Mne, My);
         var Mnl = (1 - 0.15 * Math.Pow((Mcrl / Mne), 0.4)) * Math.Pow(Mcrl / MneBar, 0.4) * MneBar;
+        output?.AddResult("Mnl", Mnl, "Eq.F3.2-2");
         return Mnl;
     }
 
-    public static double EqF3_2__3(double Mne, double My, double Mcrl)
+    public static double EqF3_2__3(double Mne, double My, double Mcrl, Output? output = null)
     {
         var MneBar = Math.Min(Mne, My);
         var Lambdal = Math.Sqrt(MneBar / Mcrl);
+        output?.AddResult("Lambdal", Lambdal, "Eq.F3.2-3");
         return Lambdal;
     }
     
     
-    public static double EqF4__1(double My)
+    public static double EqF4__1(double My, Output? output = null)
     {
         var Mnd = My;
+        output?.AddResult("Mnd", Mnd, "Eq.F4-1");
         return Mnd;
     }
     
-    public static double EqF4__2(double Mcrd, double My)
+    public static double EqF4__2(double Mcrd, double My, Output? output = null)
     {
         var Mnd = (1 - 0.22 * Math.Pow(Mcrd / My, 0.5)) * Math.Pow((Mcrd / My), 0.5) * My;
+        output?.AddResult("Mnd", Mnd, "Eq.F4-2");
         return Mnd;
     }
     
     
-    public static double EqF4__5(double My, double Mcrd)
+    public static double EqF4__5(double My, double Mcrd, Output? output = null)
     {
         var Lambdad = Math.Sqrt(My / Mcrd);
+        output?.AddResult("Lambdad", Lambdad, "Eq.F4-5");
         return Lambdad;
     }
     
-    public static double GlobalBucklingStressFn(double Fy, double Fcre)
+    public static double GlobalBucklingStressFn(double Fy, double Fcre, Output? output = null)
     {
 
+        double Fn;
         if (Fcre >= 2.78 * Fy) 
         {
-            var Fn = EqF2_1__3(Fy);
+            Fn = EqF2_1__3(Fy);
+            output?.AddResult("Fn", Fn, "Eq.F2.1-3");
             return Fn;
         } 
         if ((Fcre < 2.78 * Fy) & (Fcre > 0.56 * Fy))
         {
-            var Fn = EqF2_1__4(Fy, Fcre);
+            Fn = EqF2_1__4(Fy, Fcre);
+            output?.AddResult("Fn", Fn, "Eq.F2.1-4");
             return Fn;
         }
-        return EqF2_1__5(Fcre);
+        Fn = EqF2_1__5(Fcre);
+        output?.AddResult("Fn", Fn, "Eq.F2.1-5");
+            
+        return Fn;
     }
     
-    public static double GlobalBucklingStrengthMne(double Fy, double Fcre, double Sfc, double My)
+    public static double GlobalBucklingStrengthMne(double Fy, double Fcre, double Sfc, double My, Output? output = null)
     {
-        var Fn = GlobalBucklingStressFn(Fy, Fcre);
+        var Fn = GlobalBucklingStressFn(Fy, Fcre, output);
 
         var Mne = EqF2_1__1(Sfc, Fn, My);
+        output?.AddResult("Mne", Mne, "Eq.F2.1-1");
 
         return Mne;
     }
     
-    public static double AvailableGlobalBucklingStrengthMne(double Mne, string designMethod)
+    public static double AvailableGlobalBucklingStrengthMne(double Mne, string designMethod, Output? output = null)
     {
    
         Dictionary<string, double> SafetyResistanceFactors =  
@@ -107,24 +125,30 @@ public static class ChapterFFlexure
         SafetyResistanceFactors.Add("LRFD", 0.90);
         SafetyResistanceFactors.Add("LSD", 0.90);
 
-        var aMne = AISIS100.Core.CalculateAvailableStrength(Mne, designMethod, SafetyResistanceFactors);
+        var aMne = AISIS100.Core.CalculateAvailableStrength(Mne, designMethod, SafetyResistanceFactors, output);
 
         return aMne;
     }
     
-    public static double LocalBucklingStrengthMnl(double Mne, double My, double Mcrl)
+    public static double LocalBucklingStrengthMnl(double Mne, double My, double Mcrl, Output? output = null)
     {
         var Lambdal = EqF3_2__3(Mne, My, Mcrl);
+        output?.AddResult("Lambdal", Mne, "Eq.F3.2-3");
 
+        double Mnl;
         if (Lambdal <= 0.776) 
         {
-            var Mnl = EqF3_2__1(Mne);
+            Mnl = EqF3_2__1(Mne);
+            output?.AddResult("Mnl", Mnl, "Eq.F3.2-1");
             return Mnl;
-        } 
-        return EqF3_2__2(Mcrl, Mne, My);
+        }
+        Mnl = EqF3_2__2(Mcrl, Mne, My);
+        output?.AddResult("Mnl", Mnl, "Eq.F3.2-2");
+        
+        return Mnl;
     }
 
-    public static double AvailableLocalBucklingStrengthMnl(double Mnl, string designMethod)
+    public static double AvailableLocalBucklingStrengthMnl(double Mnl, string designMethod, Output? output = null)
     {
    
         Dictionary<string, double> SafetyResistanceFactors =  
@@ -134,24 +158,29 @@ public static class ChapterFFlexure
         SafetyResistanceFactors.Add("LRFD", 0.90);
         SafetyResistanceFactors.Add("LSD", 0.90);
 
-        var aMnl = AISIS100.Core.CalculateAvailableStrength(Mnl, designMethod, SafetyResistanceFactors);
+        var aMnl = AISIS100.Core.CalculateAvailableStrength(Mnl, designMethod, SafetyResistanceFactors, output);
 
         return aMnl;
     }
     
-    public static double DistortionalBucklingStrengthMnd(double My, double Mcrd)
+    public static double DistortionalBucklingStrengthMnd(double My, double Mcrd, Output? output = null)
     {
-        var lambdad = EqF4__5(My, Mcrd);
+        var Lambdad = EqF4__5(My, Mcrd);
 
-        if (lambdad <= 0.561) 
+        double Mnd;
+        if (Lambdad <= 0.561) 
         {
-            var mnd = EqF4__1(My);
-            return mnd;
+            Mnd = EqF4__1(My);
+            output?.AddResult("Mnd", Mnd, "Eq.F4-1");
+            return Mnd;
         } 
-        return EqF4__2(Mcrd, My);
+        Mnd = EqF4__2(Mcrd, My);
+        output?.AddResult("Mnd", Mnd, "Eq.F4-2");
+
+        return Mnd;
     }
     
-    public static double AvailableDistortionalBucklingStrengthMnd(double Mnd, string designMethod)
+    public static double AvailableDistortionalBucklingStrengthMnd(double Mnd, string designMethod, Output? output = null)
     {
    
         Dictionary<string, double> SafetyResistanceFactors =  
@@ -161,7 +190,7 @@ public static class ChapterFFlexure
         SafetyResistanceFactors.Add("LRFD", 0.90);
         SafetyResistanceFactors.Add("LSD", 0.90);
 
-        var aMnd = AISIS100.Core.CalculateAvailableStrength(Mnd, designMethod, SafetyResistanceFactors);
+        var aMnd = AISIS100.Core.CalculateAvailableStrength(Mnd, designMethod, SafetyResistanceFactors, output);
 
         return aMnd;
     }
