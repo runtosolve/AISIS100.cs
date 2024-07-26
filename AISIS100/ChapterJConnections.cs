f804d8f4ba0bf80b7dbb5fda1d4ed707260529f0
Cris Moen <cris.moen@runtosolve.com>
2024-07-26 12:36:49 +0300

    add equation numbers Chapter J

--- AISIS100/ChapterJConnections.cs
+++ AISIS100/ChapterJConnections.cs
@@ -2,45 +2,49 @@ namespace AISIS100;
 
 public class ChapterJConnections
 {
-    public static double EqJ4_4_1__1(double tc, double d, double Fu2, string units)
+    public static double EqJ4_4_1__1(double tc, double d, double Fu2, string units, Output? output = null)
     {
 
         if (units == "inches")
         {
             var alpha = 1.0;
             var Pnot = 0.85 * tc * d * Fu2 * (1.63 * Math.Pow(alpha * tc, 0.18));
+            output?.AddResult("Pnot", Pnot, "Eq.J4.4.1-1");
             return Pnot;
         }
         else
         {
             var alpha = 0.0394;
             var Pnot = 0.85 * tc * d * Fu2 * (1.63 * Math.Pow(alpha * tc, 0.18));
+            output?.AddResult("Pnot", Pnot, "Eq.J4.4.1-1");
             return Pnot;
         }
         
     }
 
     
-    public static double EqJ4_4_2__1(double t1, double dPrimew, double fu1)
+    public static double EqJ4_4_2__1(double t1, double dPrimew, double Fu1, Output? output = null)
     {
 
-        var Pnov = 0.90 * t1 * dPrimew * fu1;
+        var Pnov = 0.90 * t1 * dPrimew * Fu1;
+        output?.AddResult("Pnov", Pnov, "Eq.J4.4.2-1");
 
         return Pnov;
         
     }
     
-    public static double EqJ4_4_2__3(double dh, double tw, double t1, double dw)
+    public static double EqJ4_4_2__3(double dh, double tw, double t1, double dw, Output? output = null)
     {
 
         var dPrimew = Math.Min(dh + 2 * tw + t1, dw);
+        output?.AddResult("dPrimew", dPrimew, "Eq.J4.4.2-3");
 
         return dPrimew;
         
     }
     
     
-    public static double AvailablePulloutStrength(double Pnot, string designMethod)
+    public static double AvailablePulloutStrength(double Pnot, string designMethod, Output? output = null)
 
     {
         
@@ -51,7 +55,7 @@ public class ChapterJConnections
         safetyResistanceFactors.Add("LRFD", 0.55);
         safetyResistanceFactors.Add("LSD", 0.45);
         
-        var aPnot = AISIS100.Core.CalculateAvailableStrength(Pnot, designMethod, safetyResistanceFactors);
+        var aPnot = AISIS100.Core.CalculateAvailableStrength(Pnot, designMethod, safetyResistanceFactors, output);
 
         return aPnot;
 
@@ -60,7 +64,7 @@ public class ChapterJConnections
  
 
 
-    public static double AvailablePulloverStrength(double Pnov, string designMethod)
+    public static double AvailablePulloverStrength(double Pnov, string designMethod, Output? output = null)
 
     {
         
@@ -71,7 +75,7 @@ public class ChapterJConnections
         safetyResistanceFactors.Add("LRFD", 0.55);
         safetyResistanceFactors.Add("LSD", 0.40);
         
-        var aPnov = AISIS100.Core.CalculateAvailableStrength(Pnov, designMethod, safetyResistanceFactors);
+        var aPnov = AISIS100.Core.CalculateAvailableStrength(Pnov, designMethod, safetyResistanceFactors, output);
 
         return aPnov;
 
