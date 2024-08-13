dbdbe90c4a70a7954632e481fe392d03ac8d7196
Cris Moen <cris.moen@runtosolve.com>
2024-08-13 10:01:09 -0400

    local buckling strength tests

--- AISIS100.Tests/ChapterECompressionTests.cs
+++ AISIS100.Tests/ChapterECompressionTests.cs
@@ -13,6 +13,31 @@ public class ChapterECompressionTests
     }
     
     [Test]
+    public void EqE2__2_IsCorrect()
+    {
+        var output = new Output();
+        var Fn = AISIS100.ChapterECompression.EqE2__2(1.35, 50.0, output);
+
+        var expected = 23.36;
+        var actual = Fn;
+        Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
+        Assert.That(output.GetResult("Fn").Equation, Is.EqualTo("Eq.E2-2"));
+    }    
+    
+    [Test]
+    public void EqE2__3_IsCorrect()
+    {
+        var output = new Output();
+        var Fn = AISIS100.ChapterECompression.EqE2__3(1.35, 50.0, output);
+
+        var expected = 24.12;
+        var actual = Fn;
+        Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
+        Assert.That(output.GetResult("Fn").Equation, Is.EqualTo("Eq.E2-3"));
+    } 
+        
+    
+    [Test]
     public void EqE2__4_IsCorrect()
     {
         var output = new Output();
@@ -23,6 +48,46 @@ public class ChapterECompressionTests
         Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
         Assert.That(output.GetResult("Lambdac").Equation, Is.EqualTo("Eq.E2-4"));
     }
+  
+        
+    [Test]
+    public void  EqE3_2__1_IsCorrect()
+    {
+        var output = new Output();
+        var Pnl = AISIS100.ChapterECompression. EqE3_2__1(9.43, output);
+
+        var expected = 9.43;
+        var actual = Pnl;
+        Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
+        Assert.That(output.GetResult("Pnl").Equation, Is.EqualTo("Eq.E3.2-1"));
+    }      
+    
+    [Test]
+    public void  EqE3_2__2_IsCorrect()
+    {
+        var output = new Output();
+        var Pnl = AISIS100.ChapterECompression. EqE3_2__2(6.41, 9.43, output);
+
+        var expected = 7.04;
+        var actual = Pnl;
+        Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
+        Assert.That(output.GetResult("Pnl").Equation, Is.EqualTo("Eq.E3.2-2"));
+    }        
+
+        
+        
+    [Test]
+    public void  EqE3_2__3_IsCorrect()
+    {
+        var output = new Output();
+        var Lambdal = AISIS100.ChapterECompression. EqE3_2__3(9.43, 6.41, output);
+
+        var expected = 1.21;
+        var actual = Lambdal;
+        Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
+        Assert.That(output.GetResult("Lambdal").Equation, Is.EqualTo("Eq.E3.2-3"));
+    }    
+        
     
     [Test]
     public void EqE4__9_IsCorrect()
@@ -68,4 +133,24 @@ public class ChapterECompressionTests
         Assert.That(output.GetResult("Fn").Value, Is.EqualTo(expectedFn).Within(1.0).Percent);
         
     }
+    
+    [Test]
+    public void LocalBucklingStrengthPnl_IsCorrect()
+    {
+        var output = new Output();
+        var Pnl = AISIS100.ChapterECompression.LocalBucklingStrengthPnl(9.43, 6.41, output);
+
+        var expectedPnl = 7.04;
+        Assert.That(Pnl, Is.EqualTo(expectedPnl).Within(1.0).Percent);
+        
+        var expectedLambdal = 1.21;
+        Assert.That(output.GetResult("Lambdal").Value, Is.EqualTo(expectedLambdal).Within(1.0).Percent);    
+        
+    }
+    
+    
+    
+    
 }
+
+
