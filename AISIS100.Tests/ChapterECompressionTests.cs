770ac8c41192565a020ec0b0ceb137b3e85e67b4
Cris Moen <cris.moen@runtosolve.com>
2024-08-19 10:27:16 -0400

    add Pne, Pnl, Pnd tests including LRFD, ASD, LSD

--- AISIS100.Tests/ChapterECompressionTests.cs
+++ AISIS100.Tests/ChapterECompressionTests.cs
@@ -87,7 +87,43 @@ public class ChapterECompressionTests
         Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
         Assert.That(output.GetResult("Lambdal").Equation, Is.EqualTo("Eq.E3.2-3"));
     }    
-        
+    
+    [Test]
+    public void  EqE4__1_IsCorrect()
+    {
+        var output = new Output();
+        var Pnd = AISIS100.ChapterECompression. EqE4__1(20.19, output);
+
+        var expected = 20.19;
+        var actual = Pnd;
+        Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
+        Assert.That(output.GetResult("Pnd").Equation, Is.EqualTo("Eq.E4-1"));
+    } 
+    
+    [Test]
+    public void  EqE4__2_IsCorrect()
+    {
+        var output = new Output();
+        var Pnd = AISIS100.ChapterECompression. EqE4__2(5.36, 20.19,  output);
+
+        var expected = 8.09;
+        var actual = Pnd;
+        Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
+        Assert.That(output.GetResult("Pnd").Equation, Is.EqualTo("Eq.E4-2"));
+    } 
+    
+    
+    [Test]
+    public void  EqE4__5_IsCorrect()
+    {
+        var output = new Output();
+        var Lambdad = AISIS100.ChapterECompression. EqE4__5(20.19, 5.364, output);
+
+        var expected = 1.94;
+        var actual = Lambdad;
+        Assert.That(actual, Is.EqualTo(expected).Within(1.0).Percent);
+        Assert.That(output.GetResult("Lambdad").Equation, Is.EqualTo("Eq.E4-5"));
+    }    
     
     [Test]
     public void EqE4__9_IsCorrect()
@@ -135,6 +171,26 @@ public class ChapterECompressionTests
     }
     
     [Test]
+    public void AvailableGlobalBucklingStrengthPne_IsCorrect()
+    {
+        
+        var PneLRFD = AISIS100.ChapterECompression.AvailableGlobalBucklingStrengthPne(9.43, "LRFD");
+        var PneLRFDexpected = 9.43 * 0.85;
+        Assert.That(PneLRFD, Is.EqualTo(PneLRFDexpected).Within(1.0).Percent);
+        
+        var PneASD = AISIS100.ChapterECompression.AvailableGlobalBucklingStrengthPne(9.43, "ASD");
+        var PneASDexpected = 9.43 / 1.80;
+        Assert.That(PneASD, Is.EqualTo(PneASDexpected).Within(1.0).Percent);
+        
+        var PneLSD = AISIS100.ChapterECompression.AvailableGlobalBucklingStrengthPne(9.43, "LSD");
+        var PneLSDexpected = 9.43 * 0.80;
+        Assert.That(PneLSD, Is.EqualTo(PneLSDexpected).Within(1.0).Percent);
+       
+    }
+    
+    
+    
+    [Test]
     public void LocalBucklingStrengthPnl_IsCorrect()
     {
         var output = new Output();
@@ -148,8 +204,58 @@ public class ChapterECompressionTests
         
     }
     
+    [Test]
+    public void AvailableLocalBucklingStrengthPnl_IsCorrect()
+    {
+        
+        var PnlLRFD = AISIS100.ChapterECompression.AvailableLocalBucklingStrengthPnl(7.04, "LRFD");
+        var PnlLRFDexpected = 7.04 * 0.85;
+        Assert.That(PnlLRFD, Is.EqualTo(PnlLRFDexpected).Within(1.0).Percent);
+        
+        var PnlASD = AISIS100.ChapterECompression.AvailableLocalBucklingStrengthPnl(7.04, "ASD");
+        var PnlASDexpected = 7.04 / 1.80;
+        Assert.That(PnlASD, Is.EqualTo(PnlASDexpected).Within(1.0).Percent);
+        
+        var PnlLSD = AISIS100.ChapterECompression.AvailableLocalBucklingStrengthPnl(7.04, "LSD");
+        var PnlLSDexpected = 7.04 * 0.80;
+        Assert.That(PnlLSD, Is.EqualTo(PnlLSDexpected).Within(1.0).Percent);
+       
+    }
+
     
     
+    
+    [Test]
+    public void DistortionalBucklingStrengthPnd_IsCorrect()
+    {
+        var output = new Output();
+        var Pnd = AISIS100.ChapterECompression.DistortionalBucklingStrengthPnd(20.19, 5.36, output);
+
+        var expectedPnd = 8.09;
+        Assert.That(Pnd, Is.EqualTo(expectedPnd).Within(1.0).Percent);
+        
+        var expectedLambdad = 1.94;
+        Assert.That(output.GetResult("Lambdad").Value, Is.EqualTo(expectedLambdad).Within(1.0).Percent);    
+        
+    }
+    
+    [Test]
+    public void AvailableDistortionalBucklingStrengthPnd_IsCorrect()
+    {
+        
+        var PndLRFD = AISIS100.ChapterECompression.AvailableDistortionalBucklingStrengthPnd(8.09, "LRFD");
+        var PndLRFDexpected = 8.09 * 0.85;
+        Assert.That(PndLRFD, Is.EqualTo(PndLRFDexpected).Within(1.0).Percent);
+        
+        var PndASD = AISIS100.ChapterECompression.AvailableDistortionalBucklingStrengthPnd(8.09, "ASD");
+        var PndASDexpected = 8.09 / 1.80;
+        Assert.That(PndASD, Is.EqualTo(PndASDexpected).Within(1.0).Percent);
+        
+        var PndLSD = AISIS100.ChapterECompression.AvailableDistortionalBucklingStrengthPnd(8.09, "LSD");
+        var PndLSDexpected = 8.09 * 0.80;
+        Assert.That(PndLSD, Is.EqualTo(PndLSDexpected).Within(1.0).Percent);
+       
+    }
     
 }
 
